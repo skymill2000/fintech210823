23688dabd4cf0aed9a306b7c3e2abb1b63b2c0d5
skymill2000 <skymill2000@gmail.com>
2021-08-23 15:40:31 +0900

    work#1 완료

--- example_func.js
+++ example_func.js
@@ -1,12 +1,22 @@
-function multi(p1, p2) {
+let multi = function (p1, p2) {
   return p1 * p2;
-}
+};
 
 const div = (p1, p2) => {
   return p1 / p2;
+};
+
+const plus = (p1, p2) => {
+  return p1 + p2;
+};
+
+const minus = (p1, p2) => {
+  return p1 - p2;
 };
 
 //work#1 arrow function 으로 plus, minus method 기능을 작성해 주세요
 
 console.log(multi(1, 56));
 console.log(div(10, 2));
+console.log(plus(10, 2));
+console.log(minus(10, 2));
