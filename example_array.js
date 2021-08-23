00abcc65d34995307b42ee0d8dbeccd7791c8ec0
skymill2000 <skymill2000@gmail.com>
2021-08-23 16:04:43 +0900

    반복문의 다양한 활용 for for...of array.map()

--- example_array.js
+++ example_array.js
@@ -1,5 +1,14 @@
 let cars = ["sonata", "bmw", "porter", 100];
 //public String[] cars = new String[5];
-console.log(cars[0], cars[1], cars[2], cars[3]);
-cars.push("test2");
-console.log(cars);
+for (let i = 0; i < cars.length; i++) {
+  let car = cars[i];
+  console.log(car);
+}
+
+for (car of cars) {
+  console.log(car);
+}
+
+cars.map((car) => {
+  console.log(car);
+});
