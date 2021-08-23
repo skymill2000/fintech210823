a172726dd4a46c1f26d909c1143203fe2caffd38
skymill2000 <skymill2000@gmail.com>
2021-08-23 16:22:23 +0900

    javascript 템플릿 리터럴 예제

--- example_etc.js
+++ example_etc.js
@@ -5,13 +5,17 @@ let car = {
   date: new Date(),
 };
 
-// const printCar = (name, ph, sn, date) => {
-//   console.log(name, ph, sn, date);
-// };
 const printCar = (car) => {
   const { name, ph, sn, date } = car;
   //...
-  console.log(name, ph, sn, date);
+  const description =
+    "자동차의 이름은 : " +
+    name +
+    " 입니다. 자동차의 마력은 : " +
+    ph +
+    " 입니다.";
+  const templetDescription = `자동차의 이름은 : ${name} 입니다. 자동차의 마력은 : ${ph} 입니다`;
+  console.log(templetDescription);
 };
 
 printCar(car);
