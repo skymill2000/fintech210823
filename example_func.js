54cf496a40ffdf6c7c1769cf9e1717f4b74715f2
skymill2000 <skymill2000@gmail.com>
2021-08-23 15:37:05 +0900

    work#1 화살표 함수 연습

---
+++ example_func.js
@@ -0,0 +1,12 @@
+function multi(p1, p2) {
+  return p1 * p2;
+}
+
+const div = (p1, p2) => {
+  return p1 / p2;
+};
+
+//work#1 arrow function 으로 plus, minus method 기능을 작성해 주세요
+
+console.log(multi(1, 56));
+console.log(div(10, 2));
