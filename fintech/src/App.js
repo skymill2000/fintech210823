b1ccaf407d236a0c564b0224367a506e65496620
skymill2000 <skymill2000@gmail.com>
2021-08-24 13:48:47 +0900

    컴포넌트 불러오기

--- fintech/src/App.js
+++ fintech/src/App.js
@@ -1,32 +1,12 @@
 import logo from "./logo.svg";
 import "./App.css";
-
-const Greeting = <h1>hello, react</h1>;
-
-const NameTitle = () => {
-  return (
-    <>
-      <p>안녕하세요 ~ 님 React Program 입니다</p>
-    </>
-  );
-};
+import Header from "./component/Header";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>안녕하세요 src/App.js 를 불러왔습니다.</p>
-        {Greeting}
-        <NameTitle></NameTitle>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          리액트 배우기
-        </a>
+        <Header title="메인화면"></Header>
       </header>
     </div>
   );
---
+++ fintech/src/component/Header.js
@@ -0,0 +1,12 @@
+//rafce
+import React from "react";
+
+const Header = ({ title }) => {
+  return (
+    <div>
+      <p>{title}</p>
+    </div>
+  );
+};
+
+export default Header;
