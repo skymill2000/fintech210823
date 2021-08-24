dccadff9d4e89c2de165aa5c08a9787cf5dd9190
skymill2000 <skymill2000@gmail.com>
2021-08-24 15:24:40 +0900

    styled-component 추가

--- fintech/src/App.js
+++ fintech/src/App.js
@@ -1,16 +1,12 @@
-import logo from "./logo.svg";
-import "./App.css";
 import Header from "./component/Header";
 import EventExample from "./component/EventExample";
 import ListComponent from "./component/ListComponent";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Header title="메인화면"></Header>
-        <ListComponent></ListComponent>
-      </header>
+    <div>
+      <Header title="메인화면"></Header>
+      <ListComponent></ListComponent>
     </div>
   );
 }
--- fintech/src/component/Header.js
+++ fintech/src/component/Header.js
@@ -1,11 +1,21 @@
 //rafce
 import React from "react";
+import styled from "styled-components";
+
+const HeaderBlock = styled.div`
+  display: flex;
+  height: 60px;
+  border-bottom: 1px solid #000000;
+  text-align: text;
+  color: red;
+  justify-content: center;
+`;
 
 const Header = ({ title }) => {
   return (
-    <div>
+    <HeaderBlock>
       <p>{title}</p>
-    </div>
+    </HeaderBlock>
   );
 };
 
