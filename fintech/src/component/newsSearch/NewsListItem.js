ede2baa7179994f38824d7793d511b5767db7de6
skymill2000 <skymill2000@gmail.com>
2021-08-25 11:36:21 +0900

    나름 디자인...

--- fintech/src/component/newsSearch/NewsListItem.js
+++ fintech/src/component/newsSearch/NewsListItem.js
@@ -1,12 +1,21 @@
 import React from "react";
+import styled from "styled-components";
+
+const NewsListBlock = styled.div`
+  display: flex;
+  flex-direction: column;
+  border: 1px solid #000;
+  margin-bottom: 10px;
+  padding: 10px;
+`;
 
 const NewsListItem = ({ title, contents, url }) => {
   return (
-    <div>
+    <NewsListBlock>
       <p>제목 : {title}</p>
       <p>작성자 : {contents}</p>
       <a href={url}> 기사로 이동하기 </a>
-    </div>
+    </NewsListBlock>
   );
 };
 
