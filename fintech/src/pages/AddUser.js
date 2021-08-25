d31f1d85c1c8c54c2ab0b6541e7a4183630c715a
skymill2000 <skymill2000@gmail.com>
2021-08-25 16:09:31 +0900

    인증 페이지 링크 연동 완료

--- fintech/src/pages/AddUser.js
+++ fintech/src/pages/AddUser.js
@@ -4,7 +4,9 @@ import Header from "../component/Header";
 const AddUser = () => {
   const openAuthSite = () => {
     let tmpwindow = window.open("about:blank");
-    tmpwindow.location.href = "https://naver.com";
+    let clientId = "q7kH44ThJwjpvNRg0BbJvE1yxvx5X53DKz1rNgPF";
+    //본인의 클라이언트 아이디를 입력해주세요
+    tmpwindow.location.href = `https://testapi.openbanking.or.kr/oauth/2.0/authorize?response_type=code&client_id=${clientId}&redirect_uri=http://localhost:3000/authResult&scope=login inquiry transfer&state=12345678901234567890123456789012&auth_type=0`;
   };
   return (
     <div>
