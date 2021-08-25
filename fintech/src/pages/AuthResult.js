41aed59cf262cd79149739a01e6cf277175f916c
skymill2000 <skymill2000@gmail.com>
2021-08-25 16:25:12 +0900

    인증코드 화면 출력 쿼리스트링 - jsObject

--- fintech/src/pages/AuthResult.js
+++ fintech/src/pages/AuthResult.js
@@ -1,10 +1,16 @@
 import React from "react";
 import Header from "../component/Header";
+import { useLocation } from "react-router-dom";
+import queryString from "query-string";
+//쿼리스트링을 가져오기 위한 React Hook
 
 const AuthResult = () => {
+  const { search } = useLocation();
+  const { code } = queryString.parse(search);
   return (
     <div>
       <Header title="인증 결과"></Header>
+      <p>인증 코드 : {code}</p>
     </div>
   );
 };
