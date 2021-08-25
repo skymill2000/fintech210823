6a2804fb24ac901fa993d09f73082a947cee249b
skymill2000 <skymill2000@gmail.com>
2021-08-25 16:36:14 +0900

    useEffect 훅 선언 페이지 mount 시 getAccessToken Method 호출

--- fintech/src/pages/AuthResult.js
+++ fintech/src/pages/AuthResult.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "../component/Header";
 import { useLocation } from "react-router-dom";
 import queryString from "query-string";
@@ -7,10 +7,21 @@ import queryString from "query-string";
 const AuthResult = () => {
   const { search } = useLocation();
   const { code } = queryString.parse(search);
+  const [accessToken, setAccessToken] = useState("토큰 받아오기 전");
+
+  useEffect(() => {
+    getAccessToken();
+  }, []);
+
+  const getAccessToken = () => {
+    console.log("토큰을 가져옵니다");
+  };
+
   return (
     <div>
       <Header title="인증 결과"></Header>
       <p>인증 코드 : {code}</p>
+      <p>AccessToken : {accessToken}</p>
     </div>
   );
 };
