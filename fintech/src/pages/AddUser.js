26003165d3aaf1d63c4b62b477d0d09503b968e4
skymill2000 <skymill2000@gmail.com>
2021-08-25 16:05:40 +0900

    인증사이트 열기 버튼 연동

--- fintech/src/pages/AddUser.js
+++ fintech/src/pages/AddUser.js
@@ -1,7 +1,17 @@
 import React from "react";
+import Header from "../component/Header";
 
 const AddUser = () => {
-  return <div></div>;
+  const openAuthSite = () => {
+    let tmpwindow = window.open("about:blank");
+    tmpwindow.location.href = "https://naver.com";
+  };
+  return (
+    <div>
+      <Header title={"사용자 추가"}></Header>
+      <button onClick={openAuthSite}>사용자 인증</button>
+    </div>
+  );
 };
 
 export default AddUser;
