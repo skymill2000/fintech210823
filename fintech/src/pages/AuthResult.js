388acd07a4ee451fca57b544ab35aaa8ad416bc8
skymill2000 <skymill2000@gmail.com>
2021-08-25 16:45:24 +0900

    Axios 를 통한 토큰 요청 완성

--- fintech/src/pages/AuthResult.js
+++ fintech/src/pages/AuthResult.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import Header from "../component/Header";
 import { useLocation } from "react-router-dom";
 import queryString from "query-string";
+import axios from "axios";
 //쿼리스트링을 가져오기 위한 React Hook
 
 const AuthResult = () => {
@@ -15,6 +16,31 @@ const AuthResult = () => {
 
   const getAccessToken = () => {
     console.log("토큰을 가져옵니다");
+    const sendData = {
+      code: code,
+      client_id: "q7kH44ThJwjpvNRg0BbJvE1yxvx5X53DKz1rNgPF",
+      client_secret: "yVT6irMr2h4ZTHzZY7sDpbvhm1nlOzr4nP7DYRVy",
+      redirect_uri: "http://localhost:3000/authResult",
+      grant_type: "authorization_code",
+    };
+    //전달하는 정보의 JsObject 선언
+
+    const encodedSendData = queryString.stringify(sendData);
+    //전달하는 data 형식의 변경
+
+    const option = {
+      method: "POST",
+      url: "https://testapi.openbanking.or.kr/oauth/2.0/token",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      data: encodedSendData,
+    };
+
+    axios(option).then((response) => {
+      console.log(response);
+    });
+    //axios 오브젝트 설정
   };
 
   return (
