8aca8fe46ca4158110bd6ccd93263cbb5908931f
skymill2000 <skymill2000@gmail.com>
2021-08-26 11:35:08 +0900

    메인 리스트 카드 컴포넌트

--- fintech/src/pages/Main.js
+++ fintech/src/pages/Main.js
@@ -23,6 +23,7 @@ const Main = () => {
     };
     axios(option).then(({ data }) => {
       console.log(data);
+      setAccountList(data.res_list);
     });
 
     //#work4 사용자 계좌 목록 요청 만들기
@@ -31,6 +32,7 @@ const Main = () => {
   return (
     <div>
       <Header title={"메인"}></Header>
+      {accountList.map((account) => {})}
     </div>
   );
 };
