614db296914eb52883d7be044550d0fed02e3ffe
skymill2000 <skymill2000@gmail.com>
2021-08-26 11:06:38 +0900

    #work4 사용자 계좌 목록 불러오기 실습

--- fintech/src/pages/Main.js
+++ fintech/src/pages/Main.js
@@ -1,7 +1,19 @@
-import React from "react";
-import Header from "../component/Header";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
+import Header from "../component/Header";
 const Main = () => {
+  const [accountList, setAccountList] = useState([]);
+  useEffect(() => {
+    getAccountList();
+  }, []);
+
+  const getAccountList = () => {
+    const accessToken = localStorage.getItem("accessToken");
+    const userSeqNo = localStorage.getItem("userSeqNo");
+    //#work4 사용자 계좌 목록 요청 만들기
+  };
+
   return (
     <div>
       <Header title={"메인"}></Header>
