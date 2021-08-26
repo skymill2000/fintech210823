70f8a57c9898c7e4758f36a35fffbb288c3ae48e
skymill2000 <skymill2000@gmail.com>
2021-08-26 15:59:42 +0900

    사용자 거래내역 조회 기능 완료

--- fintech/src/pages/Balance.js
+++ fintech/src/pages/Balance.js
@@ -4,11 +4,13 @@ import queryString from "query-string";
 import BalanceCard from "../component/balance/BalanceCard";
 import Header from "../component/Header";
 import axios from "axios";
+import TransactionList from "../component/balance/TransactionList";
 
 const Balance = () => {
   const { search } = useLocation();
   const { fintechUseNo } = queryString.parse(search);
   const [balance, setBalance] = useState({});
+  const [transaction, setTransaction] = useState([]);
   console.log(fintechUseNo);
 
   const genTransId = () => {
@@ -19,6 +21,7 @@ const Balance = () => {
 
   useEffect(() => {
     getBalanceData();
+    getTransactionList();
   }, []);
 
   const getBalanceData = () => {
@@ -41,6 +44,31 @@ const Balance = () => {
     });
   };
 
+  const getTransactionList = () => {
+    const accessToken = localStorage.getItem("accessToken");
+    const option = {
+      method: "GET",
+      url: `/v2.0/account/transaction_list/fin_num`,
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+      params: {
+        bank_tran_id: genTransId(),
+        fintech_use_num: fintechUseNo,
+        inquiry_type: "A",
+        inquiry_base: "D",
+        from_date: "20210101",
+        to_date: "20210101",
+        sort_order: "D",
+        tran_dtime: "20210826132500",
+      },
+    };
+    axios(option).then(({ data }) => {
+      console.log(data);
+      setTransaction(data.res_list);
+    });
+  };
+
   return (
     <div>
       <Header title="잔액조회"></Header>
@@ -49,6 +77,7 @@ const Balance = () => {
         fintechNo={balance.fintech_use_num}
         balance={balance.balance_amt}
       ></BalanceCard>
+      <TransactionList transactionList={transaction}></TransactionList>
     </div>
   );
 };
