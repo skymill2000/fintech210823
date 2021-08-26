5e5dcc70c1ce9c10d8baa7b5cd4c022a26a1e010
skymill2000 <skymill2000@gmail.com>
2021-08-26 16:34:10 +0900

    qr코드 생성 완료

--- fintech/src/pages/QrCode.js
+++ fintech/src/pages/QrCode.js
@@ -1,10 +1,26 @@
 import React from "react";
 import Header from "../component/Header";
-
+import { useLocation } from "react-router-dom";
+import queryString from "query-string";
+import styled from "styled-components";
+const QRCode = require("qrcode.react");
+const QRBlock = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 1rem;
+`;
 const QrCode = () => {
+  const { search } = useLocation();
+  const { fintechUseNo } = queryString.parse(search);
+
   return (
     <div>
       <Header title={"QR코드생성"}></Header>
+      <QRBlock>
+        <QRCode value={fintechUseNo} size={256}></QRCode>
+        <p>{fintechUseNo}</p>
+      </QRBlock>
     </div>
   );
 };
