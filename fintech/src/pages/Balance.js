58ce400969c80a8547e173c078ad9a402b2e2919
skymill2000 <skymill2000@gmail.com>
2021-08-26 14:20:11 +0900

    #work5 사용자 계좌 잔액  API Axios 요청 작성

--- fintech/src/pages/Balance.js
+++ fintech/src/pages/Balance.js
@@ -1,8 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import queryString from "query-string";
 import BalanceCard from "../component/balance/BalanceCard";
 import Header from "../component/Header";
 
 const Balance = () => {
+  const { search } = useLocation();
+  const { fintechUseNo } = queryString.parse(search);
+  const [balance, setBalance] = useState();
+
+  useEffect(() => {
+    getBalanceData();
+  }, []);
+
+  const getBalanceData = () => {
+    //#work5 잔액조회 기능 구현하기 Axios 요청 작성 바랍니다.
+  };
+
   return (
     <div>
       <Header title="잔액조회"></Header>
